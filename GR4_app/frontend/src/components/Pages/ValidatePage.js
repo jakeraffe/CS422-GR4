e38ee66ed927b1410a8ca4e4df9c5209fafaea13
Arturo Vazquez <avazqu48@uic.edu>
2021-11-03 22:34:25 -0500

    ValidatePage fixed

--- GR4_app/frontend/src/components/Pages/ValidatePage.js
+++ GR4_app/frontend/src/components/Pages/ValidatePage.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import './Pages.css';
 import BubbleModule from '../Modules/BubbleModule'
 import {Container, Row, Col, Button} from "react-bootstrap";
@@ -12,14 +12,14 @@ const ValidatePage = () => {
                 <Container style={{marginTop: "5rem"}}>
                     <Row>
                         <Col > 
-                            <span className="float-left">
+                            <div className="float-left">
                                 <BubbleModule imgsrc="https://cdn-icons.flaticon.com/png/512/2586/premium/2586703.png?token=exp=1635984346~hmac=2819253c3af4aafa6fd319c4ff08e28f" text="LEARN"/> 
-                            </span>
+                            </div>
                         </Col>
                         <Col>
-                            <span className="float-right">
+                            <div className="float-right">
                                 <BubbleModule imgsrc="https://cdn-icons.flaticon.com/png/512/3666/premium/3666863.png?token=exp=1635984215~hmac=8d1a4405050793cee09c7c99e188c978" text="NEWS"/>
-                            </span> 
+                            </div> 
                         </Col>
                     </Row>
                     <Row>
@@ -34,14 +34,14 @@ const ValidatePage = () => {
                     </Row>
                     <Row>
                         <Col> 
-                            <span className="float-left">
-                            <BubbleModule imgsrc="https://cdn-icons.flaticon.com/png/512/1968/premium/1968777.png?token=exp=1635984406~hmac=3037320507734e4a03cddec2714b2f13" text="INFLUENCER ACTIVITY"/>
-                            </span>
+                            <div className="float-left">
+                                <BubbleModule imgsrc="https://cdn-icons.flaticon.com/png/512/1968/premium/1968777.png?token=exp=1635984406~hmac=3037320507734e4a03cddec2714b2f13" text="INFLUENCER ACTIVITY"/>
+                            </div>
                         </Col>
                         <Col> 
-                            <span className="float-right">
+                            <div className="float-right">
                                 <BubbleModule imgsrc="https://cdn-icons-png.flaticon.com/512/6041/6041310.png" text="SETTINGS"/>
-                            </span>
+                            </div>
                         </Col>
                     </Row>
                 </Container>
@@ -52,7 +52,7 @@ const ValidatePage = () => {
     return(
         <div>
             <h2>Welcome to the Home Page</h2>
-            <div class="button">
+            <div className="button">
                 {renderForm()}
             </div>
         </div>
