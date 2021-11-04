78da85bf89370e9097d645914358c72192fb0083
Arturo Vazquez <avazqu48@uic.edu>
2021-11-03 22:58:10 -0500

    Bubble Module now redirects to other pages

--- GR4_app/frontend/src/components/Modules/BubbleModule.js
+++ GR4_app/frontend/src/components/Modules/BubbleModule.js
@@ -1,12 +1,29 @@
 import React from "react";
 import {Button} from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+
 
 const BubbleModule = (props) => {
+    const history = useHistory();
+
+    // Onclick redirect to other pages
+    function redirectClick() {
+        if (props.text === "LEARN") {
+            history.push("/Learn");
+        } else if (props.text === "NEWS") {
+            history.push("/News");
+        } else if (props.text === "INFLUENCER ACTIVITY") {
+            history.push("/Influencer-Activity");
+        } else if (props.text === "SETTINGS") {
+            // Not implemented yet!
+        }
+    }
+    
     // All styles will be put into a css file...
     return(
-        <Button className="bg-secondary rounded-circle" style={{width: "300px", height: "300px"}}>
+        <Button className="bg-secondary rounded-circle" style={{width: "300px", height: "300px"}} onClick={redirectClick}>
             <img src={props.imgsrc} alt={props.text} style={{width: "180px", height: "180px", margin:"15px"}}/>
-            <p class="text-center">{props.text}</p>
+            <p className="text-center">{props.text}</p>
         </Button>
     )
 }
