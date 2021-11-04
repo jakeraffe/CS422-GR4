e0d8e1a309a0b90dbec55d46b7200627a4bf3c81
Arturo Vazquez <avazqu48@uic.edu>
2021-11-03 20:26:39 -0500

    Bubble Module added

--- GR4_app/frontend/src/components/Modules/BubbleModule.js
+++ GR4_app/frontend/src/components/Modules/BubbleModule.js
@@ -1,13 +1,13 @@
-import react from "react";
+import React from "react";
+import {Button} from "react-bootstrap";
 
-const BubbleModule = () => {
-
-
+const BubbleModule = (props) => {
+    // All styles will be put into a css file...
     return(
-        <div>
-            <h2>Bubble component</h2>
-            <p>More text</p>
-        </div>
+        <Button className="bg-secondary rounded-circle" style={{width: "300px", height: "300px"}}>
+            <img src={props.imgsrc} alt={props.text} style={{width: "180px", height: "180px", margin:"15px"}}/>
+            <p class="text-center">{props.text}</p>
+        </Button>
     )
 }
 
