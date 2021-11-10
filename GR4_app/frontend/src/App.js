933bf9b76fade332ddd93b149071164b39132d27
Arturo Vazquez <avazqu48@uic.edu>
2021-11-10 17:29:03 -0600

    navbar fixed

--- GR4_app/frontend/src/App.js
+++ GR4_app/frontend/src/App.js
@@ -6,17 +6,12 @@ import LearnPage from './components/Pages/LearnPage'
 import NewsPage from './components/Pages/NewsPage'
 import ValidatedResultPage from './components/Pages/ValidatedResultPage'
 import InfluencerPage from './components/Pages/InfluencerPage'
-import AboutUs from './components/Pages/AboutUsPage'
+import AboutUsPage from './components/Pages/AboutUsPage';
 
 // Modules
-import BubbleModule from './components/Modules/BubbleModule'
-import FilterModule from './components/Modules/FilterModule'
-import InfluencerCardModule from './components/Modules/InfluencerCardModule'
 import NavbarModule from './components/Modules/NavbarModule'
-import TitleBarModule from './components/Modules/TitleBarModule'
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import AboutUsPage from './components/Pages/AboutUsPage';
 
 function App() {
   return (
@@ -42,15 +37,6 @@ function App() {
           <Route exact path="/About-Us">
             <AboutUsPage/>
           </Route>
-           {/* <Route exact path="/Validate">
-            <Validate/>
-          </Route> */}
-          {/*<Route exact path="/learn">
-            <LearnCypto/>
-          </Route>
-          <Route exact path="/quizes">
-            <Quizes/>
-          </Route> */}
         </Switch>
       </Router>
     </div>
--- GR4_app/frontend/src/components/Modules/NavbarModule.js
+++ GR4_app/frontend/src/components/Modules/NavbarModule.js
@@ -1,31 +1,39 @@
 import React from "react";
-import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import "./Styles/nav.css";
 
 const NavbarModule = () => {
+  return (
+    <Navbar className="color-nav" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Validate Coin</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-    // Styles will be added to a styles css file...
-    return(
-        <Navbar className="color-nav" expand="lg">
-            <Container>
-                <Navbar.Brand href="/">Validate Coin</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-               
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <NavDropdown title="Learn" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="/Learn">Learn</NavDropdown.Item>
-                    <NavDropdown.Item href="">Quizzes</NavDropdown.Item>
-                    </NavDropdown>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="w-100 nav-justified">
+            <Nav.Item>
+              <NavDropdown title="Learn" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/Learn">Learn</NavDropdown.Item>
+                <NavDropdown.Item href="">Quizzes</NavDropdown.Item>
+              </NavDropdown>
+            </Nav.Item>
 
-                    <Nav.Link href="/News">News</Nav.Link>
-                    <Nav.Link href="/Influencer-Activity">Influencer-Activity</Nav.Link>
-                    <Nav.Link href="/About-Us">About Us</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+            <Nav.Item>
+              <Nav.Link href="/News">News</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/Influencer-Activity">
+                Influencer-Activity
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/About-Us">About Us</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavbarModule;
