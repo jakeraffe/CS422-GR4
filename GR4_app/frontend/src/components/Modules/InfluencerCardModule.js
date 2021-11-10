df300b90ab521a1dd08a416c7db02dafbabb3180
Pedro Aviles Aguirre <78123069+Paviles3@users.noreply.github.com>
2021-11-10 16:02:31 -0600

    updated the InfluencerCardModule

--- GR4_app/frontend/src/components/Modules/InfluencerCardModule.js
+++ GR4_app/frontend/src/components/Modules/InfluencerCardModule.js
@@ -1,23 +1,42 @@
 import React from "react";
-import {Card} from "react-bootstrap";
+import {Card, Row, Col} from "react-bootstrap";
+import "./Styles/influencercard.css";
 
 const InfluencerCardModule = (props) => {
 
 
     return(
-        <Card style={{width: '18rem'}}>
+        <Card style={{width: '28rem'}} className="influencer-card">
             <Card.Img variant="top" src={props.imgsrc} className="photo" fluid/>
             <Card.Body>
                 {/* Influencer name goes here */}
-                <Card.Title>{props.text}</Card.Title>
+                <Card.Title className="card-title">{props.text}</Card.Title>
                 {/* Social Media Influencer uses goes here */}
-                <Card.Subtitle>{props.text}</Card.Subtitle>
+                <Card.Subtitle className="card-subtitle">{props.text2}</Card.Subtitle>
                 <Card.Text>
-                    Info goes here
+                    <Row>
+                        <Col>
+                            <Row className="safe-coins-header">
+                                Safe Coins Promoted
+                            </Row>
+                            <Row>
+                                {props.text3}
+                            </Row>
+                        </Col>
+                        <Col>
+                            <Row className="scam-coins-header">
+                                Scam Coins Promoted
+                            </Row>
+                            <Row>
+                                {props.text4}
+                            </Row>
+                        </Col>
+                    </Row>
                 </Card.Text>
             </Card.Body>
         </Card> 
     )
+
 }
 
 export default InfluencerCardModule;
