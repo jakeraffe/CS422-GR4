04a78d75bcaf639c41829024f2d1f0bea1d218dd
jakeraffe <jraffe2@uic.edu>
2021-11-12 00:32:02 -0600

    slowly fixing

--- GR4_app/frontend/src/components/Pages/LearnPage.js
+++ GR4_app/frontend/src/components/Pages/LearnPage.js
@@ -7,7 +7,6 @@ import './Styles/LearnPage.css'
 import '../Modules/Styles/title.css'
 
 const LearnPage = () => {
-    var src_one = "hNDgcjVGHIw"
     var filter_array = {"Platform":["Twitter", "Instagram", "Facebook", "Youtube"], "Age-Range": ["0-16", "17-25", "26-35"]}
     return(
         <div>
@@ -30,8 +29,12 @@ const LearnPage = () => {
                     <FilterModule array={filter_array}/>
                 </div>
             </div>
-            <div>
-                <LearnVideoCardModule source={src_one}/>
+            <div className="grid">
+                <LearnVideoCardModule source="VYWc9dFqROI" title="Crypto for Beginners" body="Put simply, cryptocurrencies are like regular currencies except they’re entirely digital. Each individual cryptocurrency coin is fundamentally just a collection of numbers and letters. sThis sounds complicated, but it’s actually not far off from what we see with currencies today. Bills have serial numbers, and bank accounts have account numbers"/>
+                <LearnVideoCardModule source="AFhSf0l_aQQ" title="Crypto Scams: Part 1" body="he Ponzi Scheme started before crypto itself and relies on the premise of paying out old users with the money that is being brought on from the new users. There are a number of different Front businesses that will be used in order to justify a ponzi return."/>
+                <LearnVideoCardModule source="SSo_EIwHSd4" title="Block Chain Explained" body="What is a blockchain and how do they work? I'll explain why blockchains are so special in simple and plain English!"/>
+                <LearnVideoCardModule source="8uYNOoG41I0" title="Crypto and Cloud Storage" body="explore the current cloud storage landscape, define the problems with the popular services, offer the blockchain solution and crypto projects behind them, and envision a future of file storage on the blockchain."/>
+                <LearnVideoCardModule source="7F_RKFQl3pg" title="Crypto Scams: Part 2" body="savethekids.io was a disaster and after promoting it as ambassadors during the prelaunch... they quickly abandoned the project. Why did FaZe Clan members even promote this?"/>
             </div>
         </div>
     )
